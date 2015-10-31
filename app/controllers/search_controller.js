8f2c1ab3ee5f608872f8cfedfcda9e418fac9db7
omardelarosa <thedelarosa@gmail.com>
2015-10-31 14:21:32 -0400

    search controller now connects and disconnects from ES on each request

--- app/controllers/search_controller.js
+++ app/controllers/search_controller.js
@@ -2,7 +2,28 @@ var elasticSearch = require('elasticsearch');
 var conf          = require('../../config');
 var esConfig      = conf.esConfig;
 var _             = require('lodash');
-var client        = new elasticSearch.Client(esConfig);
+var client;
+
+var connectToES = function(){
+  return new Promise(function(resolve, reject) {
+    try {
+      client = new elasticSearch.Client(_.clone(esConfig));
+      resolve(client);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+var disconnectFromES = function () {
+  return new Promise(function(resolve, reject) {
+    if (client && client.close) {
+      resolve(client.close());
+    } else {
+      reject(new Error('Invalid client'));
+    }
+  });
+};
 
 module.exports = {
   byQueryString: function (req, res, next) {
@@ -10,14 +31,22 @@ module.exports = {
       res.send(400);
     }
     var q = req.params.query;
-    client.search({
-      index: 'pitchfork',
-      q: q
-    }).then(function(esResponse) {
-      res.send(esResponse.hits); 
+    connectToES().then(function(client){
+      client.search({
+        index: 'pitchfork',
+        q: q
+      }).then(function(esResponse) {
+        disconnectFromES().then(function(){
+          res.send(esResponse.hits); 
+        });
+      }, function (err) {
+        console.log('Error:');
+        console.log(err.message);
+        console.log(err.stack);
+        res.send(500);
+      });
     }, function (err) {
-      console.log('Error:');
-      console.log(err.message);
+      console.log('Error:', err.message);
       console.log(err.stack);
       res.send(500);
     });
